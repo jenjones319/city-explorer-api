e6e3eab98f7841e88c3cce656bf03446fcf96d6b
Jennifer B Jones <66266330+jenjones319@users.noreply.github.com>
2020-07-28 12:47:08 -0500

    cors

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 'use strict';
-
+ 
+// Application Dependencies
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+ 
+// Application Setup
 const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static('./public'));
-
-app.get('/hello', (request, response) => {
-  response.status(200).send('Hello');
-});
-
-app.get('/data', (request, response) => {
-  let airplanes = {
-    departure: Date.now(),
-    canFly: true,
-    pilot: 'Well Trained',
-  };
-  response.status(200).json(airplanes);
-});
-
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
-
+const PORT = process.env.PORT;
+app.use(cors());
+ 
+// Route Definitions
+app.get('/', rootHandler);
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+ 
+// Route Handlers
+function rootHandler(request, response) {
+  response.status(200).send('City Explorer back-end');
+}
+ 
+function notFoundHandler(request, response) {
+  response.status(404).json({ notFound: true });
+}
+ 
+function errorHandler(error, request, response, next) {
+  response.status(500).json({ error: true, message: error.message });
+}
+ 
+// App listener
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
